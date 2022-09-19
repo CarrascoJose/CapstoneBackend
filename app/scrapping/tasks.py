f24405f98c5a96aa87ec00beb7aaa5cbb4c916f7
CarrascoJose <jmcotema@gmail.com>
2022-09-19 20:55:23 -0300

    Modified lider url request

--- app/scrapping/tasks.py
+++ app/scrapping/tasks.py
@@ -13,11 +13,11 @@ from celery import shared_task
 from .models import Basket
 from .serializers import TestSerializer
 
-def lider_scrapper(basket, categories):
+def lider_scrapper(basket):
     cheap_basket = []
     total_price = 0
     for product in basket:
-        r = requests.get(categories[product]).text
+        r = requests.get(f'https://www.lider.cl/supermercado/search?Ns=sku.internetPrice%7C0&Ntt={product}').text
         soup = bs(r, 'lxml')
 
         most_cheap = soup.find('div',class_='product-details')
@@ -46,18 +46,13 @@ def compare(basket_id):
     basket = Basket.objects.get(id=basket_id)
     try:
         basket_serializer = TestSerializer(basket)
-        
-        #Getting the specific categories of the products, the user will select a list of them for build his basket.
-        # NOTE ESTO HAY QUE VERLO PORQUE ES DISTINTO PARA VARIOS SUPERMERCADOS, ENTONCES HAY QUE ARMAR DE ALGUNA FORMA UNA LISTA UNIVERSAL DE PRODUCTOS. ESTA POR EL MOMENTO LA HICE CON LAS CATEGORIAS DEL LIDER, QUE LAS SAQUE CON UN SCRAPER DE LAS CATEGORIAS Y LAS METI EN EL TXT PARA PROBARLO
-        with open(os.path.join(os.path.dirname(__file__), 'categories.txt')) as f:
-            categories_data = json.load(f)
 
         # Web Scrapping multiple markets
         # This has to be asynchronous and we must execute all of them simultaneously and wait for the result of all of them
         # Investigar async
         
         # Get the list with the most cheap products and the total price of the basket
-        lider = lider_scrapper(basket_serializer.data['basket']['products'], categories_data)
+        lider = lider_scrapper(basket_serializer.data['basket']['products'])
 
         # NOTE AQUI IRÍA LA COMPARACIÓN DE LOS PRECIOS
         basket.first_market = "lider"
