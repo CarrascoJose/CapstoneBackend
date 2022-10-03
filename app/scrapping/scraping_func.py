c56845e6c2f234da8e245e092580010f141d27f7
CarrascoJose <jmcotema@gmail.com>
2022-10-03 12:41:26 -0300

    Fixed render bugs and implemented exceptions to re-render with a limit of tries

--- app/scrapping/scraping_func.py
+++ app/scrapping/scraping_func.py
@@ -16,79 +16,97 @@ def get_acuenta_urls(basket):
     return [f'https://www.acuenta.cl/search?name={product}' for product in basket]
 ######################
 
-async def fetch_html(url:str, session:AsyncHTMLSession):
+async def fetch_html(url:str, session:AsyncHTMLSession, sleep: int):
     # Async coroutine to fetch and render the pages
     r = await session.get(url)
     print(f"Response received of: {url}")
-    await r.html.arender(sleep=5,timeout=20)
-    #TODO: IMPLEMENTAR CASO PARA CUANDO NO SE ALCANZA A RENDERIZAR O EXISTE UN TIMEOUT
-    #NOTE: DE REPENTE TIRA EL SIGUIENTE ERROR O ADVERTENCIA: 
-    #NOTE: "Future exception was never retrieved"
-    #NOTE: Ver esto en más detalle y mejorarlo
+    if sleep>0:
+        await r.html.arender(sleep=sleep,timeout=20)
     print(f"Rendered: {url}")
-    return r.html.raw_html
+    return r
 
-#TODO: IMPLEMENTAR MEJOR LAS EXCEPCIONES Y VER LOS CASOS EN QUE NO SE ENCUENTRA ALGUNA ETIQUETA
-# OPCIONES: SEGUIR RENDERIZANDO HASTA QUE CARGUE O DEVOLVER OTRO VALOR.
-async def lider_scraper(url: str, session: AsyncHTMLSession):
+async def get_soup(url: str, session: AsyncHTMLSession, sleep: int):
+    # Block of code to get the html and parse
+    try:
+        r = await fetch_html(url=url, session=session, sleep=sleep)
+    except Exception as e:
+        raise Exception("Error al realizar la petición")
+    else:
+        html = r.html.html
+        soup = BeautifulSoup(html,'lxml')
+        return soup 
+
+
+async def lider_scraper(url: str, session: AsyncHTMLSession, sleep: int):
     # Async coroutine to implement the lider web scraper
     try:
-        html = await fetch_html(url=url, session=session)
+        soup = await get_soup(url=url, session=session, sleep=sleep)
+        product_name = soup.find('span',class_='product-description').string
+        price = soup.find('span',class_='price-sell').string
 
-    except Exception as e:
-        print(f"Error fetching the url: {url} \t Message: {e.message}")
-        return {}
-    else:
-        # Here's the magic
-        # WebScraper
-        # Maybe change BeautifulSoup
-        try:
-            soup = BeautifulSoup(html,'lxml')
+        tries = 0
+
+        while ((product_name is None) or (price is None)) and tries<2:
+            print("Reintentando...")
+            soup = await get_soup(url=url, session=session, sleep=sleep)
             product_name = soup.find('span',class_='product-description').string
             price = soup.find('span',class_='price-sell').string
-            return {
-                'name':product_name,
-                'price':format_to_int(price)
-            }
-        except Exception as e:
-            return {
-                'message':f'Element not found. Error: {e}'
-            }
+            tries+=1
 
-async def acuenta_scraper(url: str, session: AsyncHTMLSession):
-    # Async coroutine to implement the lider web scraper
-    try:
-        html = await fetch_html(url=url, session=session)
+        if tries>1:
+            raise RuntimeError("Número de intentos de carga superados")
+
+        return {
+            'name':product_name,
+            'price':format_to_int(price)
+        }
 
     except Exception as e:
-        print(f"Error fetching the url: {url} \t Message: {e.message}")
-        return {}
-    else:
-        # Here the magic
-        try:
-            soup = BeautifulSoup(html,'lxml')
+        print(e)
+        return {
+            'name':"Producto no encontrado",
+            'price':0
+        }
+
+async def acuenta_scraper2(url: str, session: AsyncHTMLSession, sleep: int):
+    # Async coroutine to implement the lider web scraper
+    try:
+        soup = await get_soup(url=url, session=session, sleep=sleep)
+        prices = soup.find_all('p',class_='prod--default__price__current')
+
+        tries = 0
+        while len(prices)<1 and tries<2:
+            print("Reintentando...")
+            soup = await get_soup(url=url, session=session, sleep=sleep)
             prices = soup.find_all('p',class_='prod--default__price__current')
-            min_price_tag = functools.reduce(lambda a,b: a if int(re.sub('[\$,.]','',a.string)) < int(re.sub('[\$,.]','',b.text)) else b, prices)
+            tries+=1
+        
+        if tries>1:
+            raise RuntimeError("Número de intentos de carga superados")
 
-            min_price = min_price_tag.string
+        min_price_tag = functools.reduce(lambda a,b: a if int(re.sub('[\$,.]','',a.string)) < int(re.sub('[\$,.]','',b.text)) else b, prices)
 
-            product_name = min_price_tag.parent.parent.find('p',class_='prod__name').span.string
+        min_price = min_price_tag.string
 
-            return {
-                'name':product_name,
-                'price':format_to_int(min_price)
-            }
-            
-        except Exception as e:
-            return {
-                'message':f'Element not found. Error: {e}'
-            }
+        product_name = min_price_tag.parent.parent.find('p',class_='prod__name').span.string
+
+        return {
+            'name':product_name,
+            'price':format_to_int(min_price)
+        }    
+
+    except Exception as e:
+        print(e)
+        return {
+            'name':"Producto no encontrado",
+            'price':0
+        }
 
 async def market_scraper(urls):
     # Initializating session and tasks for each market and their respective urls
     session = AsyncHTMLSession()
-    lider_tasks = asyncio.gather(*[lider_scraper(url,session) for url in urls['lider']])
-    acuenta_tasks = asyncio.gather(*[acuenta_scraper(url,session) for url in urls['acuenta']])
+    lider_tasks = asyncio.gather(*[lider_scraper(url,session,0) for url in urls['lider']])
+    acuenta_tasks = asyncio.gather(*[acuenta_scraper2(url,session,3) for url in urls['acuenta']])
     
     # Gathering the tasks and await them
     results = await asyncio.gather(lider_tasks,acuenta_tasks)
