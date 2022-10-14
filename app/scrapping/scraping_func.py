994cf30e367f69cbe297d8106ad10498b32ce26a
CarrascoJose <jmcotema@gmail.com>
2022-10-14 20:17:51 -0300

    Fixed some async bugs and page renders

--- app/scrapping/scraping_func.py
+++ app/scrapping/scraping_func.py
@@ -12,37 +12,37 @@ from .utils import format_to_int, total_sum
 
 async def fetch_html(url:str, session:AsyncHTMLSession, sleep: int):
     # Async coroutine to fetch and render the pages
-    r = await session.get(url)
-    print(f"Response received of: {url}")
-    if sleep>0:
-        await r.html.arender(sleep=sleep,timeout=20)
-    print(f"Rendered: {url}")
-    return r
-
-async def get_soup(url: str, session: AsyncHTMLSession, sleep: int):
-    # Block of code to get the html and parse
     try:
-        r = await fetch_html(url=url, session=session, sleep=sleep)
+        r = await session.get(url)
+        print(f"Response received of: {url}")
+        if sleep>0:
+            await r.html.arender(sleep=sleep,timeout=30)
+        print(f"Rendered: {url}")
+        return r
     except Exception as e:
-        raise Exception("Error al realizar la petición")
-    else:
-        html = r.html.html
-        soup = BeautifulSoup(html,'lxml')
-        return soup 
+        raise Exception(e.message)
+
+def get_soup(r):
+    # Block of code to get the html and parse
+    html = r.html.raw_html
+    soup = BeautifulSoup(html,'lxml')
+    return soup 
 
 
-async def lider_scraper(url: str, amount: int, session: AsyncHTMLSession, sleep: int):
+async def lider_scraper(url: str, amount: int , session: AsyncHTMLSession, sleep: int):
     # Async coroutine to implement the lider web scraper
     try:
-        soup = await get_soup(url=url, session=session, sleep=sleep)
+        r = await fetch_html(url, session, sleep)
+        soup = get_soup(r)
         product_name = soup.find('span',class_='product-description')
         price = soup.find('span',class_='price-sell')
 
         tries = 0
 
-        while ((product_name is None) or (price is None)) and tries<2:
-            print("Reintentando...")
-            soup = await get_soup(url=url, session=session, sleep=sleep)
+        while ((product_name is None) or (price is None)) and tries<=1:
+            print(f"Reintentando...{url}")
+            r = await fetch_html(url, session, sleep)
+            soup = get_soup(r)
             product_name = soup.find('span',class_='product-description')
             price = soup.find('span',class_='price-sell')
             tries+=1
@@ -52,7 +52,7 @@ async def lider_scraper(url: str, amount: int, session: AsyncHTMLSession, sleep:
 
         return {
             'name':product_name.string,
-            'price':format_to_int(price.string) * amount
+            'price':format_to_int(price.string)*amount
         }
 
     except Exception as e:
@@ -63,16 +63,18 @@ async def lider_scraper(url: str, amount: int, session: AsyncHTMLSession, sleep:
         }
 
 async def acuenta_scraper(url: str, amount: int, session: AsyncHTMLSession, sleep: int):
-    # Async coroutine to implement the lider web scraper
+    # Async coroutine to implement the acuenta web scraper
     try:
-        soup = await get_soup(url=url, session=session, sleep=sleep)
-        prices = soup.find_all('p',class_='prod--default__price__current')
-
+        r = await fetch_html(url, session, sleep)
+        soup = get_soup(r)
+        prices = soup.find_all('p',class_='sc-jAaTju')
+        #print(soup)
         tries = 0
-        while len(prices)<1 and tries<2:
+        while len(prices)<1 and tries<=1:
             print("Reintentando...")
-            soup = await get_soup(url=url, session=session, sleep=sleep)
-            prices = soup.find_all('p',class_='prod--default__price__current')
+            r = await fetch_html(url, session, sleep)
+            soup = get_soup(r)
+            prices = soup.find_all('p',class_='sc-jAaTju')
             tries+=1
         
         if tries>1:
@@ -81,12 +83,11 @@ async def acuenta_scraper(url: str, amount: int, session: AsyncHTMLSession, slee
         min_price_tag = functools.reduce(lambda a,b: a if int(re.sub('[\$,.]','',a.string)) < int(re.sub('[\$,.]','',b.text)) else b, prices)
 
         min_price = min_price_tag.string
-
-        product_name = min_price_tag.parent.parent.find('p',class_='prod__name').span.string
+        product_name = min_price_tag.parent.find("p",class_="prod__name").span.string
 
         return {
             'name':product_name,
-            'price':format_to_int(min_price) * amount
+            'price':format_to_int(min_price)*amount
         }    
 
     except Exception as e:
@@ -98,13 +99,15 @@ async def acuenta_scraper(url: str, amount: int, session: AsyncHTMLSession, slee
 
 async def cencosud_scraper(url: str, amount: int, session: AsyncHTMLSession, sleep: int):
   try:
-    soup  = await get_soup(url, session, sleep)
+    r = await fetch_html(url, session, sleep)
+    soup = get_soup(r)
     all_items = soup.find_all('div', class_="shelf-product-island")
 
     tries = 0
     while len(all_items)<1 and tries<2:
       print("Reintentando...Jumbo...")
-      soup  = await get_soup(url, session, sleep)
+      r = await fetch_html(url, session, sleep)
+      soup = get_soup(r)
       all_items = soup.find_all('div', class_="shelf-product-island")
       tries += 1
       
@@ -131,15 +134,14 @@ async def cencosud_scraper(url: str, amount: int, session: AsyncHTMLSession, sle
 async def market_scraper(urls):
     # Initializating session and tasks for each market and their respective urls
     session = AsyncHTMLSession()
-    lider_tasks = asyncio.gather(*[lider_scraper(item["url"],item["amount"],session,0) for item in urls['lider']])
-    acuenta_tasks = asyncio.gather(*[acuenta_scraper(item["url"],item["amount"],session,3) for item in urls['acuenta']])
-    jumbo_tasks = asyncio.gather(*[cencosud_scraper(item["url"],item["amount"],session,5) for item in urls['jumbo']])
-    sisabel_tasks = asyncio.gather(*[cencosud_scraper(item["url"],item["amount"],session,5) for item in urls['santa_isabel']])
 
-    # Gathering the tasks and await them
-    results = await asyncio.gather(lider_tasks,acuenta_tasks,jumbo_tasks,sisabel_tasks)
+    # Running separate tasks
+    lider = await asyncio.gather(*[lider_scraper(item["url"],item["amount"],session,0) for item in urls['lider']])
+    acuenta = await asyncio.gather(*[acuenta_scraper(item["url"],item["amount"],session,5) for item in urls['acuenta']])
+    jumbo = await asyncio.gather(*[cencosud_scraper(item["url"],item["amount"],session,3) for item in urls['jumbo']])
+    ssisabel = await asyncio.gather(*[cencosud_scraper(item["url"],item["amount"],session,3) for item in urls['santa_isabel']])
 
-    sum_lider, sum_acuenta, sum_jumbo, sum_sisabel = total_sum(results[0]), total_sum(results[1]), total_sum(results[2]), total_sum(results[3])
+    sum_lider, sum_acuenta, sum_jumbo, sum_sisabel = total_sum(lider), total_sum(acuenta), total_sum(jumbo), total_sum(ssisabel)
 
     print(f"Lider total: {sum_lider}\n\n")
     print(f"Acuenta total: {sum_acuenta}\n\n")
